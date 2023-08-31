458fc9aee956759087fa8b28a669a41e27d29922
shuangmouou <1750432880@qq.com>
2023-09-01 07:57:13 +0800

    第二课程

--- second/second class/src/字符串和字符串的Buffer.java
+++ second/second class/src/字符串和字符串的Buffer.java
@@ -1,5 +1,9 @@
 public class 字符串和字符串的Buffer {
     public static void main(String[] args) {
+        System.out.println("Hello world");
+        StringBuffer str = new StringBuffer("123");
+        System.out.println(str.reverse());
+//https://www.runoob.com/java/java-stringbuffer.html
 
     }
 }
